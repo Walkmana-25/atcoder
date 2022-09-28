7cb60367317b7239ae7165391507d542603d6bca
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-09-28 10:29:40 +0900

    0928

--- abs類題/abc068/b/main.cpp
+++ abs類題/abc068/b/main.cpp
@@ -1,4 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    int n;
+    pair<int,int> m = make_pair(0,0);
+    cin >> n;
+    if(n==1){cout << 1 << endl;return 0;}
+    for (int j = 1; j <= n ; j++)
+    {
+        int count = 0;
+        int i = j;
+        while(i % 2 == 0 && i > 0){
+            count++;
+            i /= 2;
+        }
+        if(count > m.first){
+            m.first = count;
+            m.second = j;
+        }
+    }
+    cout << m.second << endl;
+    return 0;
+    
+    
 }
