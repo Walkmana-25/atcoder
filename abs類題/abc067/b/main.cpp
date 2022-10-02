08fc0e8074158d0a05cd91e87cebcdafc9599bb9
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-10-02 12:42:56 +0900

    1002

--- abs類題/abc067/b/main.cpp
+++ abs類題/abc067/b/main.cpp
@@ -1,4 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    int n,k;
+    cin >> n >> k;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int input;
+        cin >> input;
+        v.push_back(input);
+    }
+    sort(v.begin(), v.end(), greater<int>());
+    int sum = 0;
+    for (int i = 0; i < k; i++)
+    {
+        sum += v[i];
+    }
+    cout << sum << endl;
+    
+    
 }
