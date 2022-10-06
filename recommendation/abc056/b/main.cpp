76bda4576acc47164a03d216cfc79c7cbd7eea89
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-10-06 17:46:56 +0900

    1006

--- recommendation/abc056/b/main.cpp
+++ recommendation/abc056/b/main.cpp
@@ -1,4 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    int w,a,b;
+    cin >> w >> a >> b;
+    //a < bのとき
+    int ans = -1;
+    if(a < b){
+        ans = b - (a + w);
+    }else if(a > b){
+        ans = a - (b + w);
+    }
+    if(ans < 0){
+        ans = 0;
+    }
+    cout << ans << endl;
 }
