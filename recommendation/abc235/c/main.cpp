4f67737d5eddffbe5dca3bebd28fedec9a0c47b1
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-10-07 17:24:54 +0900

    1007

--- recommendation/abc235/c/main.cpp
+++ recommendation/abc235/c/main.cpp
@@ -1,33 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n,q;
+int main()
+{
+    int n, q;
     cin >> n >> q;
     vector<long> v(n);
-    vector<vector<long>> v2(q,vector<long>(2));
+    vector<vector<long>> v2(q, vector<long>(2));
+    map<long, vector<int>> m;
     for (int i = 0; i < n; i++)
     {
-        cin >> v[i];
+        int input;
+        cin >> input;
+        v[i] = input;
+        m[input].push_back(i);
     }
     for (int i = 0; i < q; i++)
     {
         cin >> v2[i][0] >> v2[i][1];
     }
-    for(auto i : v2){
-        long x = i[0];
-        long k = i[1];
-        int count = 0;
-        for (int j = 0; j < n; j++)
+    for (auto i : v2)
+    {
+        int x = i[0];
+        int k = i[1] - 1;
+        int ans = 0;
+        int size = m[x].size();
+
+        if (k > size - 1)
         {
-            if(v[j] == x){count++;}
-            if(count == k){cout << j + 1 << endl;break;}
+            ans = -2;
         }
-        if(count != k){cout << "-1" << endl;}
-        
+        else
+        {
+            ans = m[x].at(k);
+        }
+        cout << ans + 1 << endl;
     }
-    
-    
-
-
-    
 }
