be736fa7d21b81641446b1f3bdec7ae37ce6192e
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-11-13 19:29:28 +0900

    1113

---
+++ algo/bit_operation/4/4/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+    unsigned int a,b;
+    cin >> a >> b;
+    cout << (a & b) << endl;
+    cout << (a | b ) << endl;
+}
