5e7d0f13e5ddfeebbe84641f869b16b2b0f73346
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2022-12-05 17:18:22 +0900

    1205

---
+++ algo/bit_operation/9/5/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+    long unsigned int n, k;
+    cin >> n >> k;
+
+    bitset<31> bs(k);
+    
+    cout << bs.count() << endl;
+}
