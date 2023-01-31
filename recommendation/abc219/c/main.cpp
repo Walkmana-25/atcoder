4c23074ff3c550975513327645cd81710de56419
Walkmana-25 <50088655+Walkmana-25@users.noreply.github.com>
2023-01-31 09:01:14 +0900

    0131

--- recommendation/abc219/c/main.cpp
+++ recommendation/abc219/c/main.cpp
@@ -1,63 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 using ll=long long;
-int num(const char &a, const string &s){
-    for (int i = 0; i < 26; i++)
+int num(const char &c, const string &X){
+    for (int i = 0; i < X.size(); i++)
     {
         /* code */
-        if(s.at(i) == a){
+        if(X.at(i) == c){
             return i;
         }
-        return -1;
     }
+    return -1;
     
 }
 
 int main(){
-    string dict;
-    cin >> dict;
+    string X;
+    cin >> X;
     int n;
     cin >> n;
 
-    vector<string> v;
-    for (int i = 0; i < n; i++)
-    {
-        /* code */
-        string s;
-        cin >> s;
-        v.push_back(s);
-    }
-
+    vector<string> s;
+    vector<vector<int>> v;
+    map< vector<int>, string> m;
 
     for (int i = 0; i < n; i++)
     {
         /* code */
-        for (int j = n - 1; j > 0; j--)
-        {
-            /* code */
-            int l = min(v.at(j).size(), v.at(j - 1).size());
-            for (int k = 0; k < l; k++)
-            {
-                /* code */
-                if(num(v.at(j).at(k), dict) < num(v.at(j - 1).at(k), dict)){
-                    swap(v.at(j),v.at(j - 1));
-                }
-
-            }
-            
-        }
+        string ss;
+        cin >> ss;
+        s.push_back(ss);
         
     }
 
 
-    
-    for(auto a: v){
-        cout << a << "\n";
+    for(int i = 0; i < n; i++){
+        string ss = s.at(i);
+        vector<int> num_v;
+        for (int j = 0; j < ss.size(); j++)
+        {
+            /* code */
+            int tmp = num(ss.at(j), X);
+            num_v.push_back(tmp);
+
+        }
+        m[num_v] = ss;
+        v.push_back(num_v);
+        
+    }
+    sort(v.begin(), v.end());
+
+    for(auto a : v){
+        cout << m[a] << "\n";
     }
     
-
-
-
-
     
+
 }
